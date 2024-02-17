31c4d36573532fc76bd54b5c101e819294535fdf
csodom <csodom22@gmail.com>
2024-02-17 02:26:51 -0800

    Removed React Links, and added Mui components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/home/Home'
 
 function App() {
   return (
@@ -7,17 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>This Website is for CSODOM </h1>
-	<p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Pooop
-        </a>
+        <h2> 
+          On this website, there will be information about CSODOM's projects and other work history.
+        </h2>
+        <Home />
       </header>
     </div>
   );
---
+++ src/components/home/Home.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+
+class Home extends React.Component {
+
+    render() {
+        return (
+            <Button variant="contained">Do Nothing Button</Button>
+        )
+    }
+}
+
+export default Home;
