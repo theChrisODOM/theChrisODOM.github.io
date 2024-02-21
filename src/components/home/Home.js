75c8bd65d2a82092afe69a58626575d3ad980ded
csodom <csodom22@gmail.com>
2024-02-20 20:07:47 -0800

    Made avatar spin on click instead of slowly rotate

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Avatar from './Avatar';
 
 class Home extends React.Component {
 
@@ -6,7 +7,7 @@ class Home extends React.Component {
         return (
             <div>
                 <header className="App-header">
-                    <img src={"/profile.png"} className="App-logo" alt="my face" />
+                    <Avatar />
                     <h2>CSODOM's Personal Portfolio. </h2>
                     <p> 
                         On this website, there will be information about CSODOM's projects and other work history.
